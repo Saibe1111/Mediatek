d340590a7dce9a730da7fcb6afe7412e1174894b
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-02-19 12:31:46 +0100

    📝 Add and update documentation

--- src/persistantdata/MediatekData.java
+++ src/persistantdata/MediatekData.java
@@ -4,41 +4,69 @@ import java.util.List;
 
 import mediatek2021.*;
 
-// classe mono-instance : l'unique instance est connue de la bibliotheque
-// via une injection de dépendance dans son bloc static
 
+/**
+ * @version 1.1 - 19/02/2021
+ * @author Jean-François Brette / Manil RICHARD / Sébastien CUVELLIER
+ * MediatekData récupère les information de la base de donnée.
+ * Classe mono-instance : l'unique instance est connue de la bibliotheque via une injection de dépendance dans son bloc static
+ */
 public class MediatekData implements PersistentMediatek {
-// Jean-François Brette 01/01/2018
+	
+	/**
+	 * Injection dynamique de la dépendance dans le package stable mediatek2021.
+	 */
 	static {
-		// injection dynamique de la dépendance dans le package stable mediatek2021
 		Mediatek.getInstance().setData(new MediatekData());
 	}
 
+	/**
+	 * On bloque l'instanciation.
+	 */
 	private MediatekData() {
 	}
 
-	// renvoie la liste de tous les documents de la bibliothèque
+	
+	/**
+	 * Permet de récuperer une liste de document en provenance de la base de donnée
+	 * @param type - Le type de document que l'on veux.
+	 * @return retourne la liste de tous les documents de la bibliothèque
+	 */
 	@Override
 	public List<Document> catalogue(int type) {
 		return null;
 	}
 
-	// va récupérer le User dans la BD et le renvoie
-	// si pas trouvé, renvoie null
+
+	/**
+	 * Permet de récuper un utilisateur en provenance de la base de donnée
+	 * @param login - Login de l'utilisateur
+	 * @param password - Mot de passe de l'utilisateur
+	 * @return retourne l'utilisateur, si pas trouvé, renvoie null
+	 */
 	@Override
 	public Utilisateur getUser(String login, String password) {
 		return null;
 	}
 
-	// va récupérer le document de numéro numDocument dans la BD
-	// et le renvoie
-	// si pas trouvé, renvoie null
+	/**
+	 * Permet de récuperer un document en particulier en provenance de la base de donnée
+	 * @param numDocument - Numéro du document que nous cherchons.
+	 * @return retourne le document, si pas trouvé, renvoie null
+	 */
 	@Override
 	public Document getDocument(int numDocument) {
 		return null;
 	}
 
 	// ajoute un nouveau document - exception à définir
+	
+	/**
+	 * Permet de rajouter un document à la base de donnée.
+	 * @param type - Permet de savoir le type du document.
+	 * @param args - Contient les informations sur le document
+	 * @throws NewDocException - À définir <------
+	 */
 	@Override
 	public void newDocument(int type, Object... args) throws NewDocException {
 		// args[0] -> le titre
@@ -46,7 +74,11 @@ public class MediatekData implements PersistentMediatek {
 		// etc en fonction du type et des infos optionnelles
 	}
 
-	// supprime un document - exception à définir
+	/**
+	 * Permet de supprimer un document de la base de donnée.
+	 * @param numDoc - Numéro du document que nous cherchons à supprimer.
+	 * @throws SuppressException - À définir <------
+	 */
 	@Override
 	public void suppressDoc(int numDoc) throws SuppressException {
 		
--- src/services/InjectionServlet.java
+++ src/services/InjectionServlet.java
@@ -5,13 +5,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 /**
- * Servlet implementation class InjectionServlet
+ * @version 1.0 - 19/02/2021
+ * @author Sébastien CUVELLIER / Manil RICHARD
+ * Injection de dépendance
  */
 @WebServlet(value="/initializeResources", loadOnStartup=1)
 public class InjectionServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-       	
+    
+	/**
+     * Permet de faire l'injection de dépendance.
+     * @see HttpServlet#init ()
+     * @throws ServletException - Si il y a une erreur avec la servlet.
+     */
+	
 	@Override
 	public void init() throws ServletException {
 		super.init();
