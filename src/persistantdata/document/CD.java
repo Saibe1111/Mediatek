37b126020130bf1115eed78ea12c9fdd890bde38
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-02-28 22:33:43 +0100

    🚧 CD class

--- src/persistantdata/document/CD.java
+++ src/persistantdata/document/CD.java
@@ -2,11 +2,40 @@ package persistantdata.document;
 
 import mediatek2021.Document;
 
+/**
+ * @version 1.0 - 28/02/2021
+ * @author Manil RICHARD / Sébastien CUVELLIER
+ * Class qui définit un CD.
+ */
 public class CD implements Document{
+	
+	private String titre;
+	private String auteur;
+	
+	
+	/*
+	 * Constructeur du CD.
+	 * 
+	 * @param titre, titre du CD.
+	 * @param auteur, auteur du CD.
+	 */
+	public CD(String titre, String auteur) {
+		this.titre = titre;
+		this.auteur = auteur;
+	}
 
+
+	/*
+	 * Guetteur du CD.
+	 * 
+	 * @return, un objet qui définit un CD.
+	 */
 	@Override
 	public Object[] data() {
-		return null;
+		Object[] o = new Object[2];
+		o[0] = titre;
+		o[1] = auteur;
+		return o;
 	}
 	
 }
